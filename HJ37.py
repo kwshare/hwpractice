18ac4800fb24b22f2004f116d6c20f8ab5d52bdf
Danny Ding <dinglei8090@outlook.com>
2022-04-05 23:54:40 +0800

    update

--- HJ37.py
+++ HJ37.py
@@ -2,7 +2,19 @@
 
 
 
-
+# 递归法：
+while True:
+    try:
+        month=int(input())
+        n=month-1
+        def func(n):
+            if n<2:#基线条件
+                return 1
+            else:#递归条件
+                return func(n-1)+func(n-2)
+        print(func(n))
+    except:
+        break
 
 
 
