3cd498a7fa38d5b7798cf209a9b4eaef8d6f6435
kwshare <dinglei8090@outlook.com>
2022-10-12 16:47:38 +0800

    Update variable name

--- Test1/HJ01.py
+++ Test1/HJ01.py
@@ -1,41 +1,41 @@
 # HJ1 字符串最后一个单词的长度
 
 
+"""
+str1 = input()
+arr1 = str1.split(" ")
+arr1 = str1.split()
+n = len(arr1) - 1
+print(len(arr1[n]))
 
+简单  通过率：32.50%  时间限制：1秒  空间限制：32M
+知识点   字符串
 
-# str1 = input()
-# # arr1 = str1.split(" ")
-# arr1 = str1.split()
-# n = len(arr1) - 1
-# print(len(arr1[n]))
+描述:
+计算字符串最后一个单词的长度，单词以空格隔开，字符串长度小于5000。（注：字符串末尾不以空格为结尾）
 
-# 简单  通过率：32.50%  时间限制：1秒  空间限制：32M
-# 知识点   字符串
+输入描述：
+输入一行，代表要计算的字符串，非空，长度小于5000。
 
-# 描述:
-# 计算字符串最后一个单词的长度，单词以空格隔开，字符串长度小于5000。（注：字符串末尾不以空格为结尾）
+输出描述：
+输出一个整数，表示输入字符串最后一个单词的长度
 
-# 输入描述：
-# 输入一行，代表要计算的字符串，非空，长度小于5000。
-#
-# 输出描述：
-# 输出一个整数，表示输入字符串最后一个单词的长度
+示例1
+输入：
+hello nowcoder
 
-# 示例1
-# 输入：
-# hello nowcoder
-#
-# 输出：
-# 8
+输出：
+8
 
-# 说明：
-# 最后一个单词为nowcoder，长度为8
+说明：
+最后一个单词为nowcoder，长度为8
+"""
 
 while True:
     try:
-        s = input().split()
-        l = len(s)
-        l2 = len(s[l-1])
-        print(l2)
+        input_str = input().split()
+        str_length = len(input_str)
+        last_str = len(input_str[str_length - 1])
+        print(last_str)
     except:
         break
--- Test1/HJ02.py
+++ Test1/HJ02.py
@@ -1,52 +1,50 @@
 # HJ2 计算某字符出现次数
 
+"""
+string_1 = input()
+string_1 = string_1.upper()
+list_1 = list(string_1)
+check = input()
+check = check.upper()
+num = 0
+for word in list_1:
+    if word == check:
+        num = num + 1
 
-# string_1 = input()
-# string_1 = string_1.upper()
-# list_1 = list(string_1)
-# check = input()
-# check = check.upper()
-# num = 0
-# for word in list_1:
-#     if word == check:
-#         num = num + 1
-#
-# print(num)
+print(num)
 
 
-# 简单  通过率：30.44%  时间限制：1秒  空间限制：32M
+简单  通过率：30.44%  时间限制：1秒  空间限制：32M
+知识点 字符串 哈希
 
-# 知识点 字符串 哈希
+描述
+写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次数。（不区分大小写字母）
+数据范围： 1 <= n <= 1000
+输入描述：
+第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字符。
+输出描述：
+输出输入字符串中含有该字符的个数。（不区分大小写字母）
 
-# 描述
-# 写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次数。（不区分大小写字母）
+示例1
+输入：
+ABCabc
+A
 
-# 数据范围： 1 <= n <= 1000
+输出：
+2
+"""
 
-# 输入描述：
-# 第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字符。
-
-# 输出描述：
-# 输出输入字符串中含有该字符的个数。（不区分大小写字母）
-
-# 示例1
-# 输入：
-# ABCabc
-# A
-
-# 输出：
-# 2
 
 while True:
     try:
-        s1 = input()
-        s2 = input()
-        s3 = s1.upper()
-        s4 = s2.upper()
-        c = 0
-        for i in s3:
-            if i == s4:
-                c += 1
-        print(c)
+        input_str = input()
+        input_cha = input()
+        upper_str = input_str.upper()
+        upper_cha = input_cha.upper()
+        char_count = 0
+        for i in upper_str:
+            if i == upper_cha:
+                char_count += 1
+        print(char_count)
     except:
         break
